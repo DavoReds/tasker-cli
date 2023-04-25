3d60750cde90bf6bd2abbf11bedede9885b6cfa8
Dalia Rojas <daliarojasvallejo@gmail.com>
2023-04-25 00:25:43 -0500

    Created write_config function

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -2,6 +2,8 @@ use clap::ValueEnum;
 use serde::{Deserialize, Serialize};
 use std::default::Default;
 
+use crate::ConfigApp;
+
 /// # Configuration structure for the program
 ///
 /// **Name:** Name of the user.
@@ -31,12 +33,23 @@ impl Default for Config {
 }
 
 impl Config {
-    /// Loads configuration of the program.
+    /// Loads the program's configuration file.
     pub fn load_config() -> Result<Config, confy::ConfyError> {
         // It's abstracted away to ensure all three versions of the program
         // use the same config folder
         let cfg: Config = confy::load("tasker", "tasker_cli")?;
 
         Ok(cfg)
+    }
+
+    pub fn write_config(config: ConfigApp) -> anyhow::Result<()> {
+        let configuration = Config {
+            name: config.name,
+            language: config.language,
+        };
+
+        confy::store("tasker", "tasker_cli", configuration)?;
+
+        Ok(())
     }
 }
